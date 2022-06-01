b82bb9dee11b5ac1880969999cdd1c40caec2aca
wsosnowski <87486575+wsosnowski@users.noreply.github.com>
2022-06-01 19:50:23 +0200

    inference.py cleaning up

--- inference.py
+++ inference.py
@@ -2,6 +2,8 @@ import pickle
 with open('ingredient2ingredients.pickle', 'rb') as handle:
     ingredient2ingredients = pickle.load(handle)
 
-ingredient = "chicken"
-ingredient_idx = [idx for idx, elem in enumerate(ingredient2ingredients.keys()) if ingredient == elem][0]
-print(ingredient2ingredients[list(ingredient2ingredients.keys())[ingredient_idx]])
+if __name__ == "__main__":
+    ingredient = "chicken"
+    ingredient_idx = [idx for idx, elem in enumerate(ingredient2ingredients.keys()) if ingredient == elem][0]
+    print(ingredient2ingredients[list(ingredient2ingredients.keys())[ingredient_idx]])
+
