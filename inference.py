a85ad3bc8c68d7c608e43d6977b3ab0e0442c0df
wsosnowski <87486575+wsosnowski@users.noreply.github.com>
2022-08-23 17:29:21 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+import random
 
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -15,22 +16,53 @@ def generate_ingredient2ingredients(ingredeint2vector, token):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get top n entities given embeddint dictionary')
-    parser.add_argument("--embedding_path", type=str, default="ingredeint2vector.pickle",
+    parser.add_argument("--model_path", type=str, default="ingredeint2vector.pickle",
                         help="Pass the path to the ingredient embeddings dictionary")
     parser.add_argument("--source_token", type=str, default="chicken",
                         help='Enter the name of the ingredient for which you want to find the most similar substitutes')
     parser.add_argument("--topn", type=int, default=10,
                         help='Enter the number of substitutes you want to find for given ingredient')
+    parser.add_argument("--model_type", type=str, default="FastText",
+                        help='Enter the type of the model: either "Embedding" or "FrequentItems"')
 
     args = parser.parse_args()
 
     embedding_path = args.embedding_path
     source_substitute = args.source_token
     topn = args.topn
-
+    model_type = args.model_type
     with open(embedding_path, 'rb') as handle:
-        ingredient2ingredients = pickle.load(handle)
+        model = pickle.load(handle)
 
-    sorted_ingredients_scores = generate_ingredient2ingredients(ingredient2ingredients, source_substitute)
-    print(f"The top {topn} substitutes for given ingredient: {source_substitute}:")
-    print({k: sorted_ingredients_scores[k] for k in list(sorted_ingredients_scores)[:topn]})
+    if model_type == "Embedding":
+        sorted_ingredients_scores = generate_ingredient2ingredients(model, source_substitute)
+        print(f"The top {topn} substitutes for given ingredient: {source_substitute}:")
+        print({k: sorted_ingredients_scores[k] for k in list(sorted_ingredients_scores)[:topn]})
+    else:
+        len_fa = 5
+        used_contexts = []
+        freq_items_for_source_ingr = [(
+            model["support"][model["itemsets"][model["itemsets"] == el].index[0]],
+            el) for el in model['itemsets'] if
+            {source_substitute}.issubset(el) and len(el) == len_fa and (
+                not any([ee for ee in used_contexts if ee.issubset(el)]))]
+        freq_items_for_source_ingr.sort(key=lambda x: x[0], reverse=True)
+        select_idx = random.randint(0, 3)
+        context_ingredients = set(freq_items_for_source_ingr[select_idx][1])
+        context_ingredients.remove(source_substitute)
+        used_contexts.append(context_ingredients)
+        freq_items_context_ingr = [(model["support"][
+                                        model["itemsets"][model["itemsets"] == el].index[0]],
+                                    el)
+                                   for el in
+                                   model['itemsets'] if
+                                   context_ingredients.issubset(el) and len(el) == len_fa and not {
+                                       source_substitute}.issubset(el)]
+        freq_items_context_ingr.sort(key=lambda x: x[0], reverse=True)
+        res = []
+        for count in range(topn):
+            ss2 = set(freq_items_context_ingr[count][1])
+            for el in list(context_ingredients):
+                ss2.remove(el)
+            res.append(ss2)
+        print(source_substitute, res)
